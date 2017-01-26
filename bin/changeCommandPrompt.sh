6afb0393149c9e7f09030fa466b4f1429477da34
Daniel Chitralla <daniel.chitralla@kathmandu.co.nz>
2017-01-27 10:40:29 +1300

    added installer and actual script

---
+++ bin/changeCommandPrompt.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# This script changes the bash prompt of the aws instance it is running on.
+# Prerequirement: Machine must have access to its tag names!
+# 
+# @author Kathmandu Dev Team
+# @created 20170127
+#
+
+# Step 1: get the instance id
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+if [ "$1" = "debug" ]; then
+  echo "Instance Id: $INSTANCE_ID"
+fi
+
+# Step 2: get the region
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+
+if [ "$1" = "debug" ]; then
+  echo "Region: $REGION"
+fi
+
+# Step 3: get the value of the name tag
+NICKNAME=$(aws ec2 describe-tags --region "$REGION" --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=Name" --output=text | cut -f5)
+
+if [ "$1" = "debug" ]; then
+  echo "Nickname: $NICKNAME"
+fi
+
+# Step 4: replace the line in /etc/bashrc which defines the command prompt
+sudo sed -i '/@\\h /c\  [ "$PS1" = "\\\\s-\\\\v\\\\\\$ " ] && PS1="[\\u@$NICKNAME \\W]\\\\$ "' /etc/bashrc
---
+++ bin/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script installs and updates the aws prompt changer
+# what it does:
+# requirements:
+#
+
+
+# determine installation location, eventually create folder
+
+# download the prompt changing script to the right location, preferably via git
+git clone https://github.com/kmddevdani/awspromptchanger.git
+
+# execute the prompt changing script
+
+# install as cron job
